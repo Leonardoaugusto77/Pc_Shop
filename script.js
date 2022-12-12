158a5b3bd9571e9e2b7c1c3319fd7065e783be43
Heisen <leonardopjh30@gmail.com>
2022-12-12 16:16:17 -0300

    upadtes

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// Slider
+
+let imgs = []
+let slider = document.querySelector('#Slider')
+let s = 1
+let imgAtual
+let maxImg
+
+const preLoad = () =>{
+    for(let i = 0; i < 5; i++){
+        s
+        imgs[i] = new Image()
+        imgs[i].src = 'imgs/s' +s+ ".svg"
+        s++
+    }
+}
+
+const carregaImg = (img) =>{
+    slider.style.backgroundImage = "url(' "+imgs[img].src + " ')"
+}
+
+
+const trocaImg = ()=>{
+    imgAtual++
+    if(imgAtual>maxImg){
+        imgAtual=0
+    }
+    carregaImg(imgAtual)
+}
+
+
+window.addEventListener('load', ()=>{
+    preLoad()
+    imgAtual = 0
+    maxImg = imgs.length-1
+    slider
+    carregaImg(imgAtual)
+    setInterval(trocaImg, 3000)
+})
+
+
+
+
 document.addEventListener('click', (e)=>{
     const isDropdownBtn = e.target.classList.contains('dropdown')
 
@@ -47,3 +90,13 @@ const submit = () =>{
 Esubmit.addEventListener("click", submit)
 
 
+
+// Botão de subir
+
+
+let btntop = document.getElementById("btn-up")
+
+btntop.addEventListener("click", ()=>{
+  
+    window.scrollTo(0, 0) + 'slow'
+} )
